7d0c97eb4ef0f04b65f8f730770ae0fe92a3fa12
Faraz Ayaz <2501320@students.au.edu.pk>
2026-07-30 03:15:12 +0500

    express routing

--- src/01-routing.ts
+++ src/01-routing.ts
@@ -1,2 +1,10 @@
 import http, { IncomingMessage, ServerResponse } from "node:http";
-
+const express = require("express");
+const app = express();
+app.get("/", (req: IncomingMessage, res: ServerResponse) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Hello World\n");
+});
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
