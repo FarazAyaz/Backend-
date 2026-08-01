b0d81b33a20df00761410adc45ab506ba0f457ae
Faraz Ayaz <2501320@students.au.edu.pk>
2026-08-02 01:29:36 +0500

    prisma

--- src/server.js
+++ src/server.js
@@ -1,7 +1,13 @@
-const express = require('express');
+import express from 'express';
+import movieRoutes from './routes/movieRoutes.js';
 const app = express();
 
 const PORT = 5001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+});
+app.use('/api/movies', movieRoutes);
+
+app.get('/hello', (req, res) => {
+  res.json({ message: 'Hello Faraz' });
 });
